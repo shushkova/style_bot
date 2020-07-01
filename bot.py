d0f20f5affa65de22b2acb7afd56839c530a78d0
shushkova <shushkova.vv@gmail.com>
2020-07-02 01:28:56 +0300

    Update Bot

--- bot.py
+++ bot.py
@@ -10,12 +10,18 @@ from time import sleep
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
 PORT = 32100
 TOKEN = '1359919586:AAG8rzjvD18zcMWJqLg-7Wd6beM1j88i8MY'
 bot = Bot(token='1359919586:AAG8rzjvD18zcMWJqLg-7Wd6beM1j88i8MY')
 dp = Dispatcher(bot)
 
+
+WEBHOOK_HOST = 'https://immense-taiga-94950.herokuapp.com/' + TOKEN
+WEBHOOK_PATH = '/webhook'
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = 32102
+
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 @dp.message_handler(commands=['help'])
 async def send_menu(message: types.Message):
@@ -54,10 +60,38 @@ async def style_transfer(message: types.Message):
 def main():
     # executor.start_polling(dp)
     executor.start_webhook(listen="0.0.0.0",
-                   port=int(PORT),
-                   url_path=TOKEN)
-    executor.bot.setWebhook('https://immense-taiga-94950.herokuapp.com/' + TOKEN)
+                           port=int(PORT),
+                           url_path=TOKEN)
+    executor.bot.setWebhook()
+
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    # insert code here to run it after start
+
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+
+    # insert code here to run it before shutdown
+
+    # Remove webhook (not acceptable in some cases)
+    await bot.delete_webhook()
+
+    # Close DB connection (if used)
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    logging.warning('Bye!')
 
 
 if __name__ == '__main__':
-    main()
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
